3664ed047d179aed81948828d6977db33fbeec74
ljxxyxxg <leejy970103@gmail.com>
2023-12-04 15:47:22 +0900

    자바 첫시간 1장 2장 변수 형변환

---
+++ javaBasic/src/javaBaisc/ch01/sec06/hello.java
@@ -0,0 +1,33 @@
+package javaBaisc.ch01.sec06;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Java");
+       System.out.println(100+30-5);
+       System.out.println("100+30-5");
+	}
+	
+	public static void sum() {
+          //합계담당 메소드
+		 int[] score = {1,2,3}; //배열
+		 Box stringbox = new Box<String>();// <> 제네릭
+}
+
+	
+	public static void average() {
+        //평균 담당 메소드
+}
+	
+	
+	
+	
+}
+
+
+
+
+class Box<T>{
+	
+}
